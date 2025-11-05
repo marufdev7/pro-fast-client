aacba195caa0ed27c5208c706d5d70216f2292f9
mrfhmd7 <mrf.hmd7@gmail.com>
2025-11-05 17:54:55 +0600

    home, navbar & footer add

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router';
+import Navbar from '../pages/shared/Navbar/Navbar';
+import Footer from '../pages/shared/Footer/Footer';
 
 const RootLayout = () => {
     return (
         <div>
+            <Navbar />
             <Outlet />
+            <Footer />
         </div>
     );
 };
