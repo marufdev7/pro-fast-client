57aa2a7d508d1c87a2a12a7d83bd81e7da81794c
mrfhmd7 <mrf.hmd7@gmail.com>
2025-11-08 01:22:17 +0600

    freatures finished

--- src/assets/data/features.js
+++ src/assets/data/features.js
@@ -5,24 +5,28 @@ import callCenter from "../call-center.png";
 
 const features = [
     {
+        id: 1,
       title: "Live Parcel Tracking",
       description:
         "Stay updated in real-time with our live parcel tracking feature. From pick-up to delivery, monitor your shipment’s journey and get instant status updates for complete peace of mind.",
       img:liveTracking, 
     },
     {
+        id: 2,
       title: "Fast Delivery",
       description:
         "Experience quick and reliable delivery services. We ensure your parcels reach their destination safely and on time, every time.",
       img: fastDelivery,
      },
-     {
-        title: "100% Save Delivery",
+    {
+        id: 3,
+        title: "100% Safe Delivery",
         description:
           "We prioritize the safety of your parcels with our secure delivery services. Trust us to handle your shipments with utmost care from pick-up to drop-off.",
         img: safeDelivery,
     },
     {
+        id: 4,
       title: "24/7 Customer Support",
       description:
         "Our dedicated support team is always ready to assist you with any queries or issues — any time, any day.",
