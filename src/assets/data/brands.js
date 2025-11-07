2a24adace0947805360f47e32c4002a9be5b1144
mrfhmd7 <mrf.hmd7@gmail.com>
2025-11-07 21:11:28 +0600

    brands logo data create

---
+++ src/assets/data/brands.js
@@ -0,0 +1,19 @@
+import amazon from "../brands/amazon.png";
+import amazon_v from "../brands/amazon_vector.png";
+import casio from "../brands/casio.png";
+import moonstar from "../brands/moonstar.png";
+import randstad from "../brands/randstad.png";
+import start_people from "../brands/start-people.png";
+import start from "../brands/start.png";
+
+const brands = [
+    { id: 1, src: amazon, alt: "Amazon" },
+    { id: 2, src: amazon_v, alt: "Amazon Vector" },
+    { id: 3, src: casio, alt: "Casio" },
+    { id: 4, src: moonstar, alt: "Moon Star" },
+    { id: 5, src: randstad, alt: "Randstad" },
+    { id: 6, src: start_people, alt: "Start People" },
+    { id: 7, src: start, alt: "Start" },
+];
+
+export default brands;
