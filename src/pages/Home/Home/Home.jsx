f28080eda89a680a9355dd1c99fea1e83f29513d
mrfhmd7 <mrf.hmd7@gmail.com>
2025-11-08 22:53:56 +0600

    be merchant add

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -3,6 +3,7 @@ import Banner from '../Banner/Banner';
 import Services from '../Services/Services';
 import ClientMarquee from '../ClientMarquee/ClientMarquee';
 import Features from '../Features/Features';
+import BeMerchant from '../BeMerchant/BeMerchant';
 
 const Home = () => {
     return (
@@ -11,6 +12,7 @@ const Home = () => {
             <Services />
             <ClientMarquee />
             <Features />
+            <BeMerchant />
         </div>
     );
 };
