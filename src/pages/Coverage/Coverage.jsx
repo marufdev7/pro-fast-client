94bff51a436907e7fb6f278686f770ddfd6c2441
mrfhmd7 <mrf.hmd7@gmail.com>
2025-11-30 21:48:30 +0600

    import warehouse data

--- src/pages/Coverage/Coverage.jsx
+++ src/pages/Coverage/Coverage.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import CoverageMap from './CoverageMap';
+import { useLoaderData } from 'react-router';
 
 const Coverage = () => {
+    const warehouses = useLoaderData();
+    console.log(warehouses);
     return (
-        <div>
-            <h2>This is coverage page....</h2>
-        </div>
+        <section className="py-16">
+            <h2 className="text-3xl font-bold mb-6">
+                We are available in 64 districts
+            </h2>
+
+            <CoverageMap />
+
+            {/* Search box will be added later */}
+        </section>
     );
 };
 
