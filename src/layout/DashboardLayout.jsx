b7094038f5cc8f49fc91df45e3c46b4254372ff7
mrfhmd7 <mrf.hmd7@gmail.com>
2025-12-16 00:41:31 +0600

    install tanstack query

--- src/layout/DashboardLayout.jsx
+++ src/layout/DashboardLayout.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Outlet } from 'react-router';
 
 const dashboardLayout = () => {
     return (
@@ -11,10 +12,12 @@ const dashboardLayout = () => {
                         {/* Sidebar toggle icon */}
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor" className="my-1.5 inline-block size-4"><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"></path><path d="M9 4v16"></path><path d="M14 10l2 2l-2 2"></path></svg>
                     </label>
-                    <div className="px-4">Navbar Title</div>
+                    <div className="px-4">Dashboard</div>
                 </nav>
                 {/* Page content here */}
-                <div className="p-4">Page Content</div>
+                <div className="p-4">
+                    <Outlet />
+                </div>
             </div>
 
             <div className="drawer-side is-drawer-close:overflow-visible">
--- src/main.jsx
+++ src/main.jsx
@@ -6,15 +6,20 @@ import { router } from './router/router';
 import AOS from "aos";
 import "aos/dist/aos.css";
 import AuthProvider from './contexts/AuthContext/AuthProvider';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 AOS.init({ duration: 1000, delay: 300 });
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <div className='font-[Urbanist] '>
-            <AuthProvider>
-                <RouterProvider router={router} />
-            </AuthProvider>
+            <QueryClientProvider client={queryClient}>
+                <AuthProvider>
+                    <RouterProvider router={router} />
+                </AuthProvider>
+            </QueryClientProvider>
         </div>
     </StrictMode>,
 )
