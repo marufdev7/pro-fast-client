5dc30f58ba385cb9bbdf2195fcde7750d7b6f561
mrfhmd7 <mrf.hmd7@gmail.com>
2025-12-17 22:51:38 +0600

    Create Payment Form

--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -1,12 +1,13 @@
 import { Elements } from '@stripe/react-stripe-js';
 import React from 'react';
+import PaymentForm from './PaymentForm';
 
 const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Payment = () => {
     return (
         <Elements stripe={stripePromise}>
-
+            <PaymentForm />
         </Elements>
     );
 };
